cbe3b841a75d536d7f4cc389cfe45c20b70d7439
adhiraj23zelthy <adhiraj@zelthy.com>
2025-03-10 00:50:12 +0530

    code clean 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import { Routes, Route, Navigate } from "react-router";
-import Home from "./pages/Home";
-import { Toaster } from "react-hot-toast";
-import Loader from "./components/layout/Loader";
 import Page from "./infinite-table/main/page";
 import { NuqsAdapter } from "nuqs/adapters/react";
 
